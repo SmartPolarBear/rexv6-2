b679bfdba43dc80f3b8abbb4d88d4c5954a55fe4
SmartPolarBear <clevercoolbear@outlook.com>
2019-06-24 23:30:19 +0800

    ready to improve file system

--- include/xv6/syscall.h
+++ include/xv6/syscall.h
@@ -2,7 +2,7 @@
  * @ Author: SmartPolarBear
  * @ Create Time: 2019-06-01 23:56:40
  * @ Modified by: SmartPolarBear
- * @ Modified time: 2019-06-22 00:16:32
+ * @ Modified time: 2019-06-24 22:58:42
  * @ Description:
  */
 
