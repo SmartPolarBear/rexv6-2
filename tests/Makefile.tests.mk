3d8e38a2fa955ce4b0d7474a16c82e4e8271a89b
SmartPolarBear <clevercoolbear@outlook.com>
2019-08-24 23:56:17 +0800

    makefile improvement

---
+++ tests/Makefile.tests.mk
@@ -0,0 +1,28 @@
+TARGET=$(OUTDIR)/$(TARGETNAME)
+LINKER =  -L/usr/local/lib -Wl,-Bdynamic -lboost_unit_test_framework 
+MACROS = -D'BOOST_TEST_DYN_LINK' -D'BOOST_TEST_MODULE=$(TARGETNAME)'
+all: $(TARGET)
+
+run: all
+	@chmod +x $(TARGET)
+	@./$(TARGET)
+
+$(TARGET): $(_OBJS)
+	@echo "[GCC LINKER]" $^ "->" $@
+	@$(HOST_CXX) $(_OBJS) $(LINKER) -o $@
+
+
+$(OUTDIR)/%.o: %.cc | $(OUTDIR)
+	@echo "[HOST CXX]" $< "->" $@
+	@($(HOST_CXX) $< $(HOST_CXXFLAGS) $(MACROS) -c -o $@)
+
+clean: 
+	@for o in $(_OBJS); do echo "REMOVE" $$o; rm -f $$o; done
+	@for o in $(TARGET); do echo "REMOVE" $$o; rm -f $$o; done
+
+
+$(OUTDIR):
+	@echo "MKDIR" $@
+	@mkdir -p $@
+
+.PHONY: all clean run
