899c63a21d61686e3320578c744d0bf78ea2f3d1
SmartPolarBear <clevercoolbear@outlook.com>
2019-09-05 23:27:42 +0800

    modify vector

---
+++ include/klib/internal/comp.h
@@ -0,0 +1,22 @@
+#if !defined(__INCLUDE_KLIB_INTERNAL_COMP_H)
+#define __INCLUDE_KLIB_INTERNAL_COMP_H
+
+#if !defined(__cplusplus)
+#error ONLY FOR C++
+#endif
+
+//TODO: Implement these functors
+namespace klib
+{
+template <typename T>
+struct less
+{
+};
+
+template <typename T>
+struct greater
+{
+};
+} // namespace klib
+
+#endif // __INCLUDE_KLIB_INTERNAL_COMP_H
