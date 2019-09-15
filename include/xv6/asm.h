7ca9735a355b6d7cd6493b8d326108ea62d8a4c4
SmartPolarBear <clevercoolbear@outlook.com>
2019-09-15 22:00:44 +0800

    fuck falied

--- include/xv6/asm.h
+++ include/xv6/asm.h
@@ -4,24 +4,31 @@
 extern "C"
 {
 #endif
-//
-// assembler macros to create x86 segments
-//
+    //
+    // assembler macros to create x86 segments
+    //
 
-#define SEG_NULLASM                                             \
-        .word 0, 0;                                             \
-        .byte 0, 0, 0, 0
+#define SEG_NULLASM \
+    .word 0, 0;     \
+    .byte 0, 0, 0, 0
 
-// The 0xC0 means the limit is in 4096-byte units
+// The 0b11000000 means the limit is in 4096-byte units
 // and (for executable segments) 32-bit mode.
-#define SEG_ASM(type,base,lim)                                  \
-        .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
-        .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
-                (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+#define SEG_ASM(type, base, lim)                    \
+    .word(((lim) >> 12) & 0xffff), ((base)&0xffff); \
+    .byte(((base) >> 16) & 0xff), (0x90 | (type)),  \
+        (0b11000000 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
 
-#define STA_X     0x8       // Executable segment
-#define STA_W     0x2       // Writeable (non-executable segments)
-#define STA_R     0x2       // Readable (executable segments)
+// The 0b00000000 means the limit is in 1-byte units
+// and (for executable segments) 16-bit mode.
+#define SEG_ASM16_1B(type, base, lim)               \
+    .word(((lim) >> 12) & 0xffff), ((base)&0xffff); \
+    .byte(((base) >> 16) & 0xff), (0x90 | (type)),  \
+        (0b00000000 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+
+#define STA_X 0x8 // Executable segment
+#define STA_W 0x2 // Writeable (non-executable segments)
+#define STA_R 0x2 // Readable (executable segments)
 #if defined(__cplusplus)
 }
 #endif
